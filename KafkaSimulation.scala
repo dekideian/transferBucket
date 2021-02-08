efcf9e0afba712c2b5a3385fed43b3b10dd74855
Deian Petrov <deian.petrov@nagarro.com>
2021-02-08 09:14:46 +0200

    Latest jar and kafka test

---
+++ KafkaSimulation.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011-2021 GatlingCorp (https://gatling.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package computerdatabase
+
+import io.gatling.core.Predef._
+import org.apache.kafka.clients.producer.ProducerConfig
+import scala.concurrent.duration._
+
+import com.github.mnogu.gatling.kafka.Predef._
+
+class KafkaSimulation extends Simulation {
+  val kafkaConf = kafka
+    // Kafka topic name
+    .topic("test2")
+    // Kafka producer configs
+    .properties(
+      Map(
+        ProducerConfig.ACKS_CONFIG -> "1",
+        // list of Kafka broker hostname and port pairs
+        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG -> "localhost:9093",
+
+        // in most cases, StringSerializer or ByteArraySerializer
+        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer",
+        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG ->
+          "org.apache.kafka.common.serialization.StringSerializer"))
+
+  val scn = scenario("Kafka Test")
+    .exec(
+      kafka("request")
+        // message to send
+        .send[String, String]("1", "Unu"))
+
+  setUp(scn.inject(constantUsersPerSec(1) during(2 seconds)).protocols(kafkaConf))
+
+}
